eaae7224243659ec24b4e158764341dc48fdc90b
Washington Ryan <washr.3103@gmail.com>
2024-12-19 01:48:21 -0300

    Create vercel.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
-
+// .eslintrc.js
+module.exports = {
+  // Removed 'useEslintrc' and 'extensions' options
+  extends: ['next', 'prettier'],
+  rules: {
+    // Suas regras de ESLint aqui
+  },
+};
